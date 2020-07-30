70d0c0ce737bc36f7641fa1262582f4bb1a129ab
ErshovSV <e.s.v.1983@yandex.ru>
2020-07-30 21:48:12 +0300

    add start

---
+++ MPM_neironet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MPM_neironet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
